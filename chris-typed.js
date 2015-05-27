93064882be7def2b26e87afed297384ddd893199
Christopher Zenzel <chriszenzel@outlook.com>
2015-05-27 15:46:28 -0400

    Removed based on element
    Remove based on element rel tag

--- chris-typed.js
+++ chris-typed.js
@@ -7,14 +7,14 @@
 $(document).ready(function () {
 
   $('img').bind('contextmenu', function(e) { return false; });
-  $("img").mousedown(function(){ return false; });
+  $('img').mousedown(function(){ return false; });
 
 });
 
 /* Change Favorite Icon On-The-Fly */
 $(window).load(function () {
 
- $('link [rel="icon"]').remove();
+ $('link').filter('[rel="icon"]').remove();
  $('head').append('<link href="https://images.typed.com/56fb7eb9-f972-4af4-8aab-9339391e6a85/1610917_965113540187846_2159509180776299155_n.jpg" rel="shortcut icon" type="image/png" />');
 
 });
