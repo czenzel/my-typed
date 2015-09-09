949fa84da5941062a736e1e02cd15b3491ba613c
Christopher Zenzel <chriszenzel@outlook.com>
2015-09-09 14:26:38 -0400

    Markdown Edits

--- js/chris-typed.js
+++ js/chris-typed.js
@@ -235,16 +235,12 @@ function _cms_markdown() {
 	// Load markdown elements
 	var markdownTimer = setTimeout(function() {
 		if (typeof markdown != 'undefined') {
-			if ($('#markdown-script')) {
-				$('#markdown-script').ready(function() {
-					$('[markdown="1"]').each(function(index) {
-						var myContents = $(this).html();
-						myContents = markdown.toHTML(myContents);
-						$(this).html(myContents);
-					});
-					clearInterval(markdownTimer);
-				});
-			}
+			$('[markdown="1"]').each(function(index) {
+				var myContents = $(this).html();
+				myContents = markdown.toHTML(myContents);
+				$(this).html(myContents);
+			});
+			clearInterval(markdownTimer);
 		}
 	}, 100);
 }
